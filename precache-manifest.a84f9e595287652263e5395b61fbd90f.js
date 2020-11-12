bc937281c4822199f8ef0a34534b9d6e68a10889
dawidmarczak <72689506+dawidmarczak@users.noreply.github.com>
2020-11-12 23:59:41 +0100

    Updates

---
+++ precache-manifest.a84f9e595287652263e5395b61fbd90f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f0915a5368492b1f00fa95f730457f3",
+    "url": "/StarWars/index.html"
+  },
+  {
+    "revision": "d4c6d42811b7b004901b",
+    "url": "/StarWars/static/css/main.ddf1cdb7.chunk.css"
+  },
+  {
+    "revision": "e149dd9b7c48247d5524",
+    "url": "/StarWars/static/js/2.ab28af69.chunk.js"
+  },
+  {
+    "revision": "d2ad0a2780078c2e6d2dee5c5379db6c",
+    "url": "/StarWars/static/js/2.ab28af69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4c6d42811b7b004901b",
+    "url": "/StarWars/static/js/main.a40065b3.chunk.js"
+  },
+  {
+    "revision": "d57ee16edade5b4c0855",
+    "url": "/StarWars/static/js/runtime-main.4fafaa2b.js"
+  },
+  {
+    "revision": "52ee8b598488b1ffbaa93e50cbd6a2f4",
+    "url": "/StarWars/static/media/RobotoCondensed-Regular.52ee8b59.ttf"
+  },
+  {
+    "revision": "314f74460664a81394693089fdcb8c1a",
+    "url": "/StarWars/static/media/cosmos2.314f7446.jpg"
+  }
+]);
